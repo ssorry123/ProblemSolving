1fb4ce7e4b1126e40be27c31bbea8dc0f9d55085
ssorry123 <syd03160@gmail.com>
2020-07-25 02:10:17 +0900

    ATM

---
+++ baekjoon/11339.py
@@ -0,0 +1,19 @@
+N = (int)(input())
+
+a = input().split(' ')
+P = list()
+[P.append((int)(i)) for i in a]
+P = sorted(P)   # 오름차순 정렬
+#print(P)
+
+ret = 0
+k = len(P)
+for i in P:
+    ret += (int)(i)*k
+    #print(ret)
+    k-=1
+print(ret)
+
+# 먼저 시작하는 음식일의 만드는 시간일수록
+# 많은 사람들의 기다리는 시간에 포함되므로
+# 먼저 시작하는 음식의 조리시간이 가장 짧아야 된다고 생각
