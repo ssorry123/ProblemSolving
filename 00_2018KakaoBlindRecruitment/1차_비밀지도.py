7fb0dbd7d2039b446f198db17bd5d3d6525de6ee
ssorry123 <syd03160@gmail.com>
2020-09-06 20:40:55 +0900

    비트연산

--- 00_2018KakaoBlindRecruitment/1차_비밀지도.py
+++ 00_2018KakaoBlindRecruitment/1차_비밀지도.py
@@ -20,7 +20,3 @@ def solution(n, arr1, arr2):
         answer.append(check1(ret, n))
 
     return answer
-
-
-print (solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
-solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10])
