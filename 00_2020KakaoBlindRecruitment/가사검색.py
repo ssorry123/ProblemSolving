c6808e10a5c89926fc3cf49c2688e6a7b45e6199
ssorry123 <syd03160@gmail.com>
2020-09-10 05:07:45 +0900

    트라이미사용, 효율성테스트2번째 시간초과

--- 00_2020KakaoBlindRecruitment/가사검색.py
+++ 00_2020KakaoBlindRecruitment/가사검색.py
@@ -1,7 +1,9 @@
 def solution(words, queries):
     answer = []
-    # 효율성도 본다고 하니, 해쉬처럼, 단어의 길이에 따라 리스트를 만들어보자
-    # 효율성테스트 2번 실패,,
+    # 트라이 미사용, string slice
+    # 효율성테스트 2번만 시간초과
+
+    # 단어 길이에 따른 분류
     by_len = dict()
     for w in words:
         key = len(w)
@@ -11,35 +13,31 @@ def solution(words, queries):
             by_len[key] = list()
             by_len[key].append(w)
 
-    # 중복된 쿼리에 대한 처리
+    # 메모이제이션용 변수
     cache = dict()
 
     for q in queries:
-        ''' 메모이제이션 '''
+        # 메모이제이션
         if q in cache:
             answer.append(cache[q])
             continue
+        # 쿼리의 길이에 맞는 단어가 없을 경우
+        if not len(q) in by_len:
+            answer.append(0)
+            continue
 
+        ''' 매치되는지 확인 '''
         # ?가 앞인지 뒤인지 확인
         front = True
         if q[-1] == '?':
             front = False
-
-
-        ''' 매치되는지 확인 '''
+        # 길이 확인        
         key = len(q)
-        # 쿼리 길이에 맞는 워드가 없는 경우
-        if not key in by_len:
-            answer.append(0)
-            continue
-        
-        # 해당 쿼리의 길이에 해당하는 단어가 존재할 경우
-        cnt = 0
-        
         # ?가 몇글자인지 확인
         wildcard_cnt = q.count('?')
 
-        # 와일드카드부분을 제거한 문자열 추출
+        cnt = 0
+        # 와일드카드부분을 제거한 문자열 비교
         for w in by_len[key]:
             if front:
                 if w[wildcard_cnt:] == q[wildcard_cnt:]:
@@ -53,7 +51,9 @@ def solution(words, queries):
 
     return answer
 
-print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
-                ["fro??", "????o", "fr???", "fro???", "pro?"]
-            )
+print(
+    solution(
+        ["frodo", "front", "frost", "frozen", "frame", "kakao"],
+        ["fro??", "????o", "fr???", "fro???", "pro?"]
+    )
 )
