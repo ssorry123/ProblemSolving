768075ad66c8273082b2bef6df2ed6d2a82e5593
ssorry123 <syd03160@gmail.com>
2020-09-10 22:44:55 +0900

    AC

---
+++ 프로그래머스월간코드챌린지시즌1_1/1.py
@@ -0,0 +1,17 @@
+# AC
+# numbers배열로, 가능한 모든 두 수의 조합을 만들어, 모든 조합의 합을 반환
+def dfs(start, numbers, cnt = 0,sum = 0, ret = list()):
+    if cnt == 2:
+        if not sum in ret:
+            ret.append(sum)
+        return
+
+    for i in range(start, len(numbers)):
+        dfs(i+1, numbers, cnt + 1, sum + numbers[i], ret)
+    
+
+def solution(numbers):
+    answer = []
+    ret = dfs(0, numbers, 0, 0, answer)
+    answer = sorted(answer) # 정렬하여 반환
+    return answer
