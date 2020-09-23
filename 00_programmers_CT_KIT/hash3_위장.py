bae0c23b41d8673ef4928d674fc77b99c0d4a5a7
ssorry123 <syd03160@gmail.com>
2020-09-23 19:11:48 +0900

    조합 구하는 법,,, 처음 알았다.,,

---
+++ 00_programmers_CT_KIT/hash3_위장.py
@@ -0,0 +1,25 @@
+def solution(clothes):
+    answer = 0
+    
+    spy = dict()
+    for v, k in clothes:    # 옷, 옷의 종류
+        if not k in spy:
+            spy[k] = set()
+        spy[k].add(v)
+
+
+    min_k = 1
+    max_k = len(spy)
+    
+    # idx1부터, 옷의 종류에 따른 옷의 개수를 저장
+    arr = [0] * (max_k)
+    for idx, k in enumerate(spy):
+        arr[idx] = len(spy[k])
+    print(arr)
+
+    # 0번 종류의 옷 경우의 수 => 선택안함, 하나 선택, 두개 선택, ...
+    # 1번 종류의 옷 경우의 수 => 선택안함, 하나선택, ...
+    # n번 종류의 옷 경우의 수 => ... ,
+    
+    
+    return answer
