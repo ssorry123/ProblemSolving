421af3a11e8c5f45d7dda9b1bc26d17c80cf413f
ssorry123 <syd03160@gmail.com>
2020-10-08 22:48:39 +0900

    AC_2020_10_08

---
+++ 프로그래머스월간코드챌린지시즌1/10_1.py
@@ -0,0 +1,21 @@
+import math
+# 주어진 10진수 n을 3진수로 변환하고
+# 변환된 3진수를 reverse한 후
+# reverse한 3진수를 10진수로 표현하는 문제
+def solution(n):
+    answer = 0
+
+    ret = ''
+
+    while n!=0:
+        ret += str(n%3)
+        n = math.floor(n/3)
+        print(n)
+    
+    for i in range(1, len(ret)+1):
+        answer += int(ret[-i])*(3**(i-1))
+
+    print(answer)
+    return answer
+
+solution(45)
---
+++ 프로그래머스월간코드챌린지시즌1/10_2.py
@@ -0,0 +1,46 @@
+# 간단한 압축 문제
+
+ret = [0 , 0]
+def div(arr, r, c, size):
+    global ret
+    if size == 1:
+        ret[arr[r][c]] += 1
+        return
+    
+    check = arr[r][c]
+    rediv = False
+    # 범위 검사
+    for i in range(r, r+size):
+        for j in range(c, c+size):
+            # 하나라도 다른게 있다면
+            if arr[i][j] != check:
+                rediv = True
+                break
+        if rediv==True:
+            break
+    
+    # 모두 같다면 압축,
+    if not rediv:
+        ret[arr[r][c]] += 1
+        return
+    
+    # 모두 같지 않다면, 분할해서 다시 해봄
+    half = int(size/2)
+    div(arr, r, c, half)
+    div(arr, r + half, c, half)
+    div(arr, r, c + half, half)
+    div(arr, r + half, c + half, half)
+    return
+
+
+def solution(arr):
+    answer = []
+
+    # 카운터 초기화
+    global ret
+    ret = [0,0]
+
+    div(arr, 0, 0, len(arr))
+
+    answer = ret
+    return answer
